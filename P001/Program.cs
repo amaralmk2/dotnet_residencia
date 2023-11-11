ed2f387bb6956402f5a3b487a38af121e5de9c89
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2023-11-11 15:56:48 -0300

    Adcionado as duas primeiras partes do exercicio P001

--- P001/Program.cs
+++ P001/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region
+
+int anoCarro = 2012;
+long kmRodados = 10000000L;
+double litroGasolina = 20.5;
+
+Console.WriteLine(" O carro tem " + kmRodados + "KM rodados" + "| tem " + litroGasolina + " L de combustivel " +"| O ano do carro é: "+ anoCarro);
+
+#endregion
+
+#region 
+
+double variavel;
+int var;
+
+variavel = 20.9;
+
+var = (int)variavel;
+
+Console.WriteLine("A variavel ficou com o valor: "+ var);
+
+#endregion
+
+#region 
+
+
+#endregion
