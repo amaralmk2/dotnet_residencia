57a5b230019df02c646095ce861bfa0a0278cec7
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2023-11-11 16:48:21 -0300

    Feito parte 5 do exercicio P001

--- P001/Program.cs
+++ P001/Program.cs
@@ -34,3 +34,15 @@ Console.WriteLine(x+" / "+ y + " = "+(x/y));
 
 
 #endregion
+
+#region 
+
+int a=5, b=8;
+
+if(a>b){
+    Console.WriteLine(a +" é maior que "+ b);
+}else{
+    Console.WriteLine(b + " é maior que "+ a);
+}
+
+#endregion
