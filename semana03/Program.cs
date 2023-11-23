9617405c4233c62cf9b245caeb1e133d37b4964e
alexandre amaral <alexandreamaral.gt@gmail.comgit config --global user.email alexandreamaral.gt@gmail.comgit config --global user.email alexandreamaral.gt@gmail.com>
2023-11-23 17:40:15 -0300

    exercicio feito na aula 3

---
+++ semana03/Program.cs
@@ -0,0 +1,11 @@
+﻿#region 
+   
+
+   var tupla1 = (idade: 25, nome: "Alexandre");
+
+   List<(int idade, string nome)> list = new();
+   list.Add(tupla1);
+  
+    Console.WriteLine($"Nome: {tupla1.nome}, {tupla1.idade}");
+
+#endregion
