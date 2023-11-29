784469d80485851e97b35d0631bb488f8c554080
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2023-11-29 17:24:34 -0300

    Criado Classe Estoque com atributos com Getters e Seters e o construtor

--- P003/Program.cs
+++ P003/Program.cs
@@ -1,12 +1,40 @@
-﻿class Program{
+﻿using System;
+
+class Estoque{
+
+    public int Cod_prod {set; get;}
+    public int Qtd_prod {set; get;}
+    public String Nome_prod {set; get;}
+    public double Preco_prod {set; get;}
+
+    public Estoque(int cod_prod, int qtd_prod, string nome_prod, double preco_prod){
+        
+        Cod_prod = cod_prod;
+        Qtd_prod = qtd_prod;
+        Nome_prod = nome_prod;
+        Preco_prod = preco_prod;
+
+    }
+}
+
+
+class Program{
 
     static void Main(String[] args){
         Boolean flag = false;
-        List<(int cod_prod, string nome_prod, int qtd_prod, double preco_prod)> lista = 
-        new List<(int cod_prod, string nome_prod, int qtd_prod, double preco_prod)>();
+        int op;
+        List<Estoque> lista = 
+        new List<Estoque>();
 
         do{
-            Console.WriteLine("testando");
+            Console.WriteLine("<------------------>");
+            Console.WriteLine("1 - Cadastrat Produto");
+            op = int.Parse(Console.ReadLine());
+
+            if(op == 1){
+
+            }
+
             flag = true;
         }while(!flag);
 
