cd46571ba753b422eef05be35fcca8436ceaab04
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2023-11-29 17:55:40 -0300

    Adicionado metodo para criar produto instanciando uma nova tupla, incrementado também CRUD

--- P003/Program.cs
+++ P003/Program.cs
@@ -15,6 +15,10 @@ class Estoque{
         Preco_prod = preco_prod;
 
     }
+
+    public void IncrementaCod(){
+        Cod_prod++;
+    }
 }
 
 
@@ -22,20 +26,54 @@ class Program{
 
     static void Main(String[] args){
         Boolean flag = false;
-        int op;
+        int op, qtd_prod, cod_prod;
+        double preco_prod;
+        String nome_prod, ch;
+
         List<Estoque> lista = 
         new List<Estoque>();
 
         do{
             Console.WriteLine("<------------------>");
             Console.WriteLine("1 - Cadastrat Produto");
+            Console.WriteLine("Teste listar produto.");
             op = int.Parse(Console.ReadLine());
 
             if(op == 1){
+                Boolean flag_temp = false;
+                do{
+
+                Console.WriteLine("Digite o nome do produto");
+                nome_prod = Console.ReadLine();
+
+                Console.WriteLine("Insira o preco do produto");
+                preco_prod = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Insira a quantidade de produtos");
+                qtd_prod = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Insira o codigo do produto");
+                cod_prod = int.Parse(Console.ReadLine());
+                
+                lista.Add(new Estoque(cod_prod , qtd_prod, nome_prod, preco_prod));
+
+                Console.WriteLine("Deseja Adiconar mais produtos?");
+                ch = Console.ReadLine();
+
+                if(ch == "sim" || ch == "SIM"){
+                    flag_temp = true;
+                }else{
+                    flag_temp = false;
+                }
+
+                }while(flag_temp == true);
+
 
             }
-
+            if(op == 3){
             flag = true;
+            }
+            
         }while(!flag);
 
     }
