59adfe119808e0c8f0de1193b7b97d0faea16646
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2023-11-29 17:13:59 -0300

    Criando estrutura basica do programa

---
+++ P003/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program{
+
+    static void Main(String[] args){
+        Boolean flag = false;
+        List<(int cod_prod, string nome_prod, int qtd_prod, double preco_prod)> lista = 
+        new List<(int cod_prod, string nome_prod, int qtd_prod, double preco_prod)>();
+
+        do{
+            Console.WriteLine("testando");
+            flag = true;
+        }while(!flag);
+
+    }
+}
