f3d75a23aaa10f7d61cfaaec30b5b60078de7f2a
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2023-11-30 17:28:21 -0300

    criado metodo para listar uma faixa de idade entre minima e maxima do advogado

--- prova/Program.cs
+++ prova/Program.cs
@@ -56,6 +56,19 @@ class Advogado
         Cna = cna;
         cpfsExistentes.Add(cpf);
     }
+
+    public int CalcularIdade()
+    {
+        DateTime hoje = DateTime.Now;
+        int idade = hoje.Year - Dat_nascimento.Year;
+
+        if (hoje.Month < Dat_nascimento.Month || (hoje.Month == Dat_nascimento.Month && hoje.Day < Dat_nascimento.Day))
+        {
+            idade--;
+        }
+
+        return idade;
+    }
 }
 
 class Cliente
@@ -103,8 +116,8 @@ class Program
         List<Advogado> advogados = new List<Advogado>();
         List<Cliente> clientes = new List<Cliente>();
 
-        advogados.Add(new Advogado("Pedro", DateTime.Now, "12345578901", "AE238V"));
-        clientes.Add(new Cliente("João", "12345578901", "Solteiro", "Marceneiro")); // CPF alterado aqui
+        advogados.Add(new Advogado("Pedro", new DateTime(1990, 1, 1), "12345578901", "AE238V"));
+        clientes.Add(new Cliente("João", "12345578901", "Solteiro", "Marceneiro"));
 
         try
         {
@@ -117,12 +130,12 @@ class Program
 
         foreach (Cliente cliente in clientes)
         {
-            Console.WriteLine($"Advogado: {cliente.Nome}, CPF: {cliente.Cpf}");
+            Console.WriteLine($"Cliente: {cliente.Nome}, CPF: {cliente.Cpf}");
         }
 
         try
         {
-            advogados.Add(new Advogado("Maria", DateTime.Now, "98765432101", "OAB456"));
+            advogados.Add(new Advogado("Maria", new DateTime(1985, 5, 10), "98765432101", "OAB456"));
         }
         catch (InvalidOperationException e)
         {
@@ -131,7 +144,24 @@ class Program
 
         foreach (Advogado advogado in advogados)
         {
-            Console.WriteLine($"Advogado: {advogado.Nome}, CPF: {advogado.Cpf}, CNA: {advogado.Cna}");
+            Console.WriteLine($"Advogado: {advogado.Nome}, CPF: {advogado.Cpf}, CNA: {advogado.Cna}, Idade: {advogado.CalcularIdade()}, Nascimento: {advogado.Dat_nascimento.ToString("dd/MM/yyyy")}");
         }
+        
+        
+        Console.WriteLine("Digite uma idade minima e na sequencia uma idade Maxima.");
+        int idadeMinima = 45;
+        int idadeMaxima = 100;
+
+        List<Advogado> advogadosComIdade = advogados.FindAll(a => a.CalcularIdade() >= idadeMinima && a.CalcularIdade() <= idadeMaxima);
+
+        Console.WriteLine($"\nAdvogados com idade entre {idadeMinima} e {idadeMaxima} anos:");
+
+        foreach (Advogado advogado in advogadosComIdade){
+            Console.WriteLine($"Advogado: {advogado.Nome}, CPF: {advogado.Cpf}, CNA: {advogado.Cna}, Idade: {advogado.CalcularIdade()}, Nascimento: {advogado.Dat_nascimento.ToString("dd/MM/yyyy")}");
+        }
+       
+
+        
+
     }
-}
+        }
